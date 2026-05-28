f8aa44398c4ec404efbd0c79dc22aec4f9b0671f
MuhammadNoumanAfzal <mnoumanafzal4@gmail.com>
2026-05-28 19:38:31 +0500

    feat: add ScrollToTop component to enhance navigation experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import AppRouter from "./app/router/AppRouter";
+import ScrollToTop from "./app/router/ScrollToTop";
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <ScrollToTop />
+      <AppRouter />
+    </>
+  );
 }
